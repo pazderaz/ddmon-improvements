fdd603b9646499b9bebccb895bbb6033b5466fa7
Zdenek Pazdera <zden.pazdera@gmail.com>
2026-03-30 15:09:58 +0200

    Added showcase mailbox case descriptions

--- examples/showcase/lib/showcase.ex
+++ examples/showcase/lib/showcase.ex
@@ -5,16 +5,20 @@ defmodule Showcase do
 
   def test_mail_wait(message \\ @default_message) do
     pid = test_mailbox(Mailbox, message)
+    # This will correctly wait just long enough for the mailbox to process the messages.
     :sys.get_status(pid)
   end
 
   def test_monitored_wait(message \\ @default_message) do
     pid = test_mailbox(MonitoredMailbox, message)
+    # This will sometimes give enough time to process the messages, but often not enough
+    # because we are testing the proxy, not the actual mailbox.
     :sys.get_status(pid)
   end
 
   def test_monitored_wait_corrected(message \\ @default_message) do
     pid = test_mailbox(MonitoredMailbox, message)
+    # The helper now correctly waits for the actual mailbox (not the proxy) to process the messages.
     DDMon.Test.get_status(pid)
   end
 
